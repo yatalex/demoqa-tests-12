71e4675b320e63deb74d493d607afabd9f72ad5c
yatalex <aleksgeo@yandex.ru>
2022-04-19 16:56:56 +0200

    upd

--- src/test/java/yatalex/TextBoxTests.java
+++ src/test/java/yatalex/TextBoxTests.java
@@ -1,13 +1,43 @@
 package yatalex;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void fillFormTest(){
-        open ("https://demoqa.com/text-box");
+
+        String name = "Alex";
+
+        open ("/text-box");
+
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail").setValue("alex@alex.com");
+        $("[id=currentAddress]").setValue("Street2");
+        $("[id=permanentAddress]").setValue("Street3");
+        $("[id=submit]").click();
+
+
+        $("[id=output]").shouldHave(text(name), text("alex@alex.com"),
+                text("Street2"), text("Street3"));
+        $("[id=output] [id=name]").shouldHave(text(name));
+        $("[id=output]").$("[id=name]").shouldHave(text(name));
+
+        $("[id=permanentAddress]").shouldHave(text(name));
 
 
 
